51d7d782ad70ac94706e5bc5efd72eb72438cbbd
Marco Santana <marco.santana@h2o.ai>
2021-12-19 19:29:09 -0300

    improve source Upper Alphabet implementation

--- passgen/gen.swift
+++ passgen/gen.swift
@@ -12,7 +12,7 @@ let sourceAlphabet = SourceItem(
   list: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"])
 let sourceUpperAlphabet = SourceItem(
   type: .upperAlphabet,
-  list: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"])
+  list: sourceAlphabet.list.map {$0.uppercased()})
 let sourceNumeric = SourceItem(
   type: .numeric,
   list: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
