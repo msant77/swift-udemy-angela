93c502d972a3f3eb760360377b4c50520b1fdadd
Marco Santana <marco.santana@h2o.ai>
2021-12-19 19:15:28 -0300

    add code along and challenges for sections 1-6

---
+++ passgen/gen.swift
@@ -0,0 +1,42 @@
+enum CharacterType {
+  case alphabet, upperAlphabet, numeric, special
+}
+
+struct SourceItem {
+  var type: CharacterType = .alphabet
+  var list: [String] = [""] 
+}
+
+let sourceAlphabet = SourceItem(
+  type: .alphabet,
+  list: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"])
+let sourceUpperAlphabet = SourceItem(
+  type: .upperAlphabet,
+  list: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"])
+let sourceNumeric = SourceItem(
+  type: .numeric,
+  list: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+let sourceSpecial = SourceItem(
+  type: .special,
+  list: ["!", "@", "#", "%", "&", "*", "£", "∞", "§", "¶", "•", "ª", "º", "¡"])
+
+let source: [SourceItem] = [sourceAlphabet, sourceUpperAlphabet, sourceNumeric, sourceSpecial], defaultSize = 8
+
+func get() -> String {
+  let item: SourceItem = source.randomElement() ?? sourceNumeric
+  return item.list.randomElement() ?? sourceNumeric.list[0]
+}
+
+var password = "", arguments: [String] = Array(CommandLine.arguments.dropFirst())
+
+func generate() {
+  if (arguments.isEmpty) { arguments.append(String(defaultSize)) }
+  for _ in 1...(Int(arguments[0]) ?? defaultSize) {
+    let character = get()
+    password += character
+  }
+      
+  print("\n", password, "\n")
+}
+
+generate()
---
+++ swift101.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+var greeting = "Hey yo, playground"
+print(greeting)
+
+print("concat calculation of 1 + 5:  \(1 + 5)")
+print(Int.random(in: 1...12))
